65a213084853038d2e1f9a4467e6b86298a558ed
citisolo <citisolo@gmail.com>
2018-11-16 12:22:17 +0000

    made some minor additions for testing and deployment

--- index.js
+++ index.js
@@ -1,369 +1,300 @@
 const fs = require('fs');
 const walk = require('walkdir');
-const readChunk = require('read-chunk');
-const fileType = require('file-type');
-const pdf = require('pdf-parse');
 const PDF = require('pdfinfo');
 const Path = require('path');
 const PDFImage = require('pdf-image').PDFImage;
-const jsonfile = require('jsonfile');
-const util = require('util');
-//const ASQ = require("asynquence");
-//const buffer = readChunk.sync('unicorn.png', 0, 4100);
+const EventEmitter = require('events');
+const loki = require('lokijs');
 
-var NUMFILES = 0;
 
-const renameFileCallback =  function (err){
-  if (err) throw err;
-  //console.log('Move complete.');
+
+
+var books = null;
+var db = null;
+
+function databaseInitialize() {
+	collection = db.getCollection('books');
+	if (collection === null) {
+		books = db.addCollection('books');
+	} else {
+		books = collection;
+	}
 }
 
-const writeFileCallback = function(res){
-  console.log('write successful: %s', newPath)
-  fs.rename(imagePath, newImagePath, renameFileCallback);
 
+
+function createNewPath(oldPath, prefix) {
+
+	let newPath = Path.basename(oldPath);
+	newPath = prefix + newPath;
+
+	return newPath;
 }
 
-const getImageCallback = function (imagePath) {
-  //create thumbnail and save to metadata
-  let newImagePath = createNewPath(imagePath, outdir);
-  meta.thumbnail = newImagePath;
-  //console.log(imagePath);
-  //console.log(meta)
-  //write the metadata to file
-  jsonfile.writeFile(newPath, meta)
-    .then(writeFileCallback)
-    .catch(function(err){
-      console.log('write unsuccessful:%s', filepath)
-      console.log(err);
-    })
+class DataEmitter extends EventEmitter {
+	// constructor(){
+	//   this.JOBS = [];
+	//
+	//   this.on('addJob', function(){
+	//     this.JOBS.push("0");
+	//   })
+	//
+	//   this.on('removeJob',function(){
+	//       this.JOBS.pop();
+	//       if (this.JOBS.length < 1){
+	//         this.emit('jobsFinished');
+	//       }
+	//     })
+	//
+	//   this.on('jobsFinished',function(func){
+	//       func();
+	//   })
+	// }
 }
 
-const getMetaDataCallback = function(err, meta){
-    //console.log(filepath);
+const metaThumbnailEmitter = new DataEmitter()
 
-    if (err){
-      throw err;
-    }
+const metaDataEmitter = new DataEmitter();
 
-    return meta;
-    //add filepath to metada
-    // meta.path = filepath;
-    //
-    // //Create a new path to save metadata file
-    // let newPath = createNewPath(filepath, outdir);
-    // newPath = newPath + ".json";
-
-
-    // var pdfImage = new PDFImage(filepath);
-    // pdfImage.convertPage(0)
-    // .then(getImageCallback)
-    // .catch(function (err) {
-    //   console.log(filepath)
-    //   console.log(err)
-    // })
-
-    //console.log('pdf info:', meta)
-  }
-
-function createNewPath(oldPath, prefix){
-
-  let newPath = Path.basename(oldPath);
-  newPath = prefix + newPath;
-
-  return newPath;
-}
-
-function extractMetadata(filepath, outdir){
-  //Get Metadata
-  //let dataBuffer = fs.readFileSync(filepath);
-  var pdf = PDF(filepath);
-  //console.log('Reached');
-  pdf.info(getMetaDataCallback)
-}
-
-// function extractThumbnail(filepath, outdir) {
-//   var pdfImage = new PDFImage(filepath);
-//   pdfImage.convertPage(0)
-//   .then(function (imagePath) {
-//     let newImagePath = Path.basename(imagePath);
-//     newImagePath = outdir + newImagePath;
-//     fs.rename(imagePath, newImagePath, function (err){
-//       if (err) throw err;
-//       //console.log('Move complete.');
-//     });
-//     console.log(imagePath);
-//   })
-//   .catch(function (err) {
-//     console.log(filepath)
-//     console.log(err)
-//   })
+// class DataEmit extends EventEmitter {
+// 	constructor(outdir){
+//     this.outdir = outdir;
+// 	}
+//
+// 	onFileInput(data){
+//
+// 	}
+//
+// 	onFileFinished(metadata){
+//
+// 	}
+//
+// 	onThumbnailInput(metadata){
+//
+// 	}
+//
+// 	onThumbnailImageMade(metada){
+//
+// 	}
+//
+// 	onThumbnailFinished(metadata){
+//
+// 	}
+//
+// 	onPipelineFinished(metadata){
+//
+// 	}
+//
 // }
 
-function processPDF (filepath, outdir){
-  if (!fs.existsSync(filepath) || !fs.existsSync(outdir)) {
-    throw new Error("filepath and outdir must be defined");
-    return;
-  }
+function scanFile(filepath, outdir) {
 
-  extractMetadata(filepath, outdir);
-  //extractThumbnail(path);
+	metaThumbnailEmitter.on('jobFinished', function(metadata) {
+		console.log("WRITING DATABASE...");
+		books.insert(metadata);
+		db.saveDatabase();
+	});
+
+	metaThumbnailEmitter.on('imageMade', function(metadata) {
+		let newImagePath = createNewPath(metadata.imagePath, outdir);
+		fs.rename(metadata.imagePath, newImagePath, function(err) {
+			if (err != null) {
+				console.log(err);
+			}
+			console.log('write successful: %s', newImagePath);
+			metadata = Object.assign(metadata, {
+				imagePath: newImagePath
+			});
+			//metadata.imagePath = newImagePath;
+			metaThumbnailEmitter.emit('jobFinished', metadata);
+		});
+	});
+
+	metaThumbnailEmitter.on('input', function(metadata) {
+		let filepath = metadata.path;
+		var pdfImage = new PDFImage(filepath);
+		pdfImage.convertPage(0)
+			.then(function(imagePath) {
+				//metadata.imagePath = imagePath
+				metadata = Object.assign(metadata, {
+					imagePath: imagePath
+				});
+				metaThumbnailEmitter.emit('imageMade', metadata);
+			})
+			.catch(function(err) {
+				console.log(filepath);
+				console.log(err);
+			});
+	});
+
+	metaDataEmitter.on('jobFinished', function(metadata) {
+		//console.log(metadata);
+		metaThumbnailEmitter.emit('input', metadata);
+	});
+
+	metaDataEmitter.on('input', function(file) {
+		var pdf = PDF(file);
+		pdf.info(function(err, metadata) {
+			//console.log(file);
+			if (err) {
+				//FIXME: handle this better
+				console.log(file);
+				console.log(err);
+				return;
+			}
+			//meta.path = file;
+			metadata = Object.assign(metadata, {
+				path: file
+			});
+			metaDataEmitter.emit('jobFinished', metadata);
+
+		});
+
+	});
+
+	if (!fs.lstatSync(filepath).isDirectory()) {
+		let ext = Path.extname(filepath);
+		if (ext === ".pdf") {
+			metaDataEmitter.emit('input', filepath);
+		}
+	}
+}
+
+function walkdir(dir, outdir) {
+
+	metaThumbnailEmitter.on('jobFinished', function(metadata) {
+		console.log("WRITING DATABASE...");
+		books.insert(metadata);
+		db.saveDatabase();
+	});
+
+	metaThumbnailEmitter.on('imageMade', function(metadata) {
+		let newImagePath = createNewPath(metadata.imagePath, outdir);
+		fs.rename(metadata.imagePath, newImagePath, function(err) {
+			if (err != null) {
+				console.log(err);
+			}
+			console.log('write successful: %s', newImagePath);
+			metadata = Object.assign(metadata, {
+				imagePath: newImagePath
+			});
+			//metadata.imagePath = newImagePath;
+			metaThumbnailEmitter.emit('jobFinished', metadata);
+		});
+	});
+
+	metaThumbnailEmitter.on('input', function(metadata) {
+		let filepath = metadata.path;
+		var pdfImage = new PDFImage(filepath);
+		pdfImage.convertPage(0)
+			.then(function(imagePath) {
+				//metadata.imagePath = imagePath
+				metadata = Object.assign(metadata, {
+					imagePath: imagePath
+				});
+				metaThumbnailEmitter.emit('imageMade', metadata);
+			})
+			.catch(function(err) {
+				console.log(filepath);
+				console.log(err);
+			});
+	});
+
+	metaDataEmitter.on('jobFinished', function(metadata) {
+		//console.log(metadata);
+		metaThumbnailEmitter.emit('input', metadata);
+	});
+
+	metaDataEmitter.on('input', function(file) {
+		var pdf = PDF(file);
+		pdf.info(function(err, metadata) {
+			//console.log(file);
+			if (err) {
+				//FIXME: handle this better
+				console.log(file);
+				console.log(err);
+				return;
+			}
+			//meta.path = file;
+			metadata = Object.assign(metadata, {
+				path: file
+			});
+			metaDataEmitter.emit('jobFinished', metadata);
+
+		});
+
+	});
+
+	var walkEmitter = walk(dir);
+
+	walkEmitter.on('file', function(filepath, stat) {
+		if (!fs.lstatSync(filepath).isDirectory()) {
+			let ext = Path.extname(filepath);
+			if (ext === ".pdf") {
+				metaDataEmitter.emit('input', filepath);
+			}
+		}
+	})
 
 }
 
+function scanPDF(file, outdir) {
+	if (!fs.existsSync(file) || !fs.existsSync(outdir)) {
+		throw new Error("<file> and <outdir> must be defined");
+	}
 
-function aysnc walkdir(dir, outdir, func){
-  let filesToScan = [];
-  const walkDir = util.promisify(walk);
-  let walkDirPromise = walkDir(dir)
-    .then(function(filepath, stat){
-      if(!fs.lstatSync(filepath).isDirectory()){
-        const fileTypeInfo = fileType(readChunk.sync(filepath, 0, 4100));
-        //console.log("found: %s", filepath)
-        //console.log(fileType(buffer))
-        if (fileTypeInfo != null && fileTypeInfo.ext === "pdf"){
-          filesToScan.push(filepath);
-          //processPDF(filepath, outdir);
-        }
-        // if (fileTypeInfo != null && fileTypeInfo.ext === "png"){
-        //   fs.unlink(filepath , function (err) {
-        //     if (err) throw err;
-        //     console.log('%s: Deletion sucessful.', filepath);
-        //   });
-        // }
-      }
-    });
+	let databasePath = createNewPath("database.json", outdir);
+	db = new loki(databasePath, {
+		autoload: true,
+		autoloadCallback: databaseInitialize,
+		// autosave: true,
+		// autosaveInterval: 4000
+	});
+
+	var interval = setInterval(function() {
+		if (books === null) {
+			console.log("waiting for database...");
+		} else {
+			clearInterval(interval);
+			if (!fs.lstatSync(file).isDirectory()) {
+				let ext = Path.extname(file);
+				if (ext === ".pdf") {
+					scanFile(file, outdir);
+				}
+			}
+		}
+	}, 1500);
+}
+
+//TODO: implement
+function scanEPUBdir() {
 
 }
 
 function scanPDFdir(dir, outdir, func) {
-    var filesToScan = [];
-    if (!fs.existsSync(dir) || !fs.existsSync(outdir)) {
-      throw new Error("dir and outdir must be defined");
-      return;
-    }
+	if (!fs.existsSync(dir) || !fs.existsSync(outdir)) {
+		throw new Error("<dir> and <outdir> must be defined");
+	}
 
-    console.log("WALKING DIRECTORY...");
-    var emitter = walk(dir);
+	let databasePath = createNewPath("database.json", outdir);
+	db = new loki(databasePath, {
+		autoload: true,
+		autoloadCallback: databaseInitialize,
+		// autosave: true,
+		// autosaveInterval: 4000
+	});
 
-    emitter.on('file', function(filepath, stat){
-      if(!fs.lstatSync(filepath).isDirectory()){
-        const fileTypeInfo = fileType(readChunk.sync(filepath, 0, 4100));
-        //console.log("found: %s", filepath)
-        //console.log(fileType(buffer))
-        if (fileTypeInfo != null && fileTypeInfo.ext === "pdf"){
-          filesToScan.push(filepath);
-          //processPDF(filepath, outdir);
-        }
-        // if (fileTypeInfo != null && fileTypeInfo.ext === "png"){
-        //   fs.unlink(filepath , function (err) {
-        //     if (err) throw err;
-        //     console.log('%s: Deletion sucessful.', filepath);
-        //   });
-        // }
-      }
-    })
+	console.log("WALKING DIRECTORY...");
 
-    emitter.on('end', function (filename, stat){
-      console.log("GETTING FILE METADA...");
-      let files = filesToScan;
-      let res = files.map(function(filepath){
-          return new Promise((resolve, reject) => {
-            var pdf = PDF(filepath)
-            pdf.info(function(err, meta){
-                //console.log(filepath);
-
-                if (err){
-                  reject(err);
-                }
-                meta.path = filepath;
-                return resolve(meta);
-            })
-          })
-          // let options = {
-          //     max: 0,
-          // }
-          //
-          // let dataBuffer = fs.readFileSync(filepath)
-          // return pdf(fs.readFileSync(filepath), options).then(function(data){
-          //     return {path:filepath, numpages: data.numpages, info: data.info, metadata: data.metadata};
-          // });
-      });
-
-      Promise.all(res)
-      .then(function(files){
-        console.log("CREATING FILE THUMBNAILS...");
-         res = files.map(function(data){
-           console.log(data.path);
-           let filepath = data.path
-           var pdfImage = new PDFImage(filepath);
-           return pdfImage.convertPage(0)
-                 .then(function(imagePath){
-                   data.imagePath = imagePath
-                   return data;
-                 })
-                 .catch(function (err) {
-                   console.log(filepath)
-                   console.log(err)
-                 })
-         })
-
-         Promise.all(res)
-         .then(function(files){
-           console.log("MOVING THUMBNAILS...");
-           let res = files.map(function(file){
-               let newImagePath = createNewPath(file.imagePath, outdir);
-
-               return new Promise((resolve, reject) => {
-                   fs.rename(file.imagePath, newImagePath,  function (err) {
-                     if (err != null) {
-                         console.log(err);
-                         reject(err);
-                     }
-                     console.log('write successful: %s', newImagePath);
-                     file.imagePath = newImagePath;
-                     return resolve(file);
-                   });
-                 });
-               });
-
-           Promise.all(res)
-           .then(function(metadata){
-             console.log("WRITING DATABASE...");
-             //console.log(files);
-             let databasePath = createNewPath("database.json", outdir);
-             jsonfile.writeFile(databasePath, metadata)
-               .then(function(data){
-                 console.log('write successful: %s', databasePath)
-               })
-               .catch(function(err){
-                 console.log('json file write unsuccessful: %s',databasePath)
-                 console.log(err);
-               })
-           });
-         })
-      })
-    })
-
-    // ASQ(function(done){
-    //   console.log("WALKING DIRECTORY...");
-    //   var emitter = walk(dir);
-    //
-    //   emitter.on('file', function(filepath, stat){
-    //     if(!fs.lstatSync(filepath).isDirectory()){
-    //       const fileTypeInfo = fileType(readChunk.sync(filepath, 0, 4100));
-    //       //console.log("found: %s", filepath)
-    //       //console.log(fileType(buffer))
-    //       if (fileTypeInfo != null && fileTypeInfo.ext === "pdf"){
-    //         filesToScan.push(filepath);
-    //         //processPDF(filepath, outdir);
-    //       }
-    //       // if (fileTypeInfo != null && fileTypeInfo.ext === "png"){
-    //       //   fs.unlink(filepath , function (err) {
-    //       //     if (err) throw err;
-    //       //     console.log('%s: Deletion sucessful.', filepath);
-    //       //   });
-    //       // }
-    //     }
-    //   })
-    //
-    //   emitter.on('end', function (filename, stat){
-    //     done(filesToScan);
-    //   })
-    // })
-    // .then(function(done, files){
-    //   //console.log(files);
-    //   console.log("GETTING FILE METADA...");
-    //
-    //   let res = files.map(function(filepath){
-    //
-    //       return new Promise((resolve, reject) => {
-    //         var pdf = PDF(filepath)
-    //         pdf.info(function(err, meta){
-    //             //console.log(filepath);
-    //
-    //             if (err){
-    //               reject(err);
-    //             }
-    //             meta.path = filepath;
-    //             return resolve(meta);
-    //         })
-    //       })
-    //       // let options = {
-    //       //     max: 0,
-    //       // }
-    //       //
-    //       // let dataBuffer = fs.readFileSync(filepath)
-    //       // return pdf(fs.readFileSync(filepath), options).then(function(data){
-    //       //     return {path:filepath, numpages: data.numpages, info: data.info, metadata: data.metadata};
-    //       // });
-    //   });
-    //
-    //   Promise.all(res)
-    //   .then(function(data){
-    //     done(data);
-    //   })
-    // })
-    // .then(function(done, files){
-    //   console.log("CREATING FILE THUMBNAILS...");
-    //    res = files.map(function(data){
-    //      let filepath = data.path
-    //      var pdfImage = new PDFImage(filepath);
-    //      return pdfImage.convertPage(0)
-    //            .then(function(imagePath){
-    //              data.imagePath = imagePath
-    //              return data;
-    //            })
-    //            .catch(function (err) {
-    //              console.log(filepath)
-    //              console.log(err)
-    //            })
-    //    })
-    //
-    //    Promise.all(res)
-    //    .then(function(res){
-    //      done(res);
-    //    })
-    // })
-    // .then(function(done, files){
-    //   console.log("MOVING THUMBNAILS...");
-    //   let res = files.map(function(file){
-    //     let newImagePath = createNewPath(file.imagePath, outdir);
-    //
-    //     return new Promise((resolve, reject) => {
-    //         fs.rename(file.imagePath, newImagePath,  function (err) {
-    //           if (err != null) {
-    //               console.log(err);
-    //               reject(err);
-    //           }
-    //           console.log('write successful: %s', newImagePath);
-    //           file.imagePath = newImagePath;
-    //           return resolve(file);
-    //         });
-    //       });
-    //     });
-    //
-    //     Promise.all(res)
-    //     .then(function(data){
-    //       done(data);
-    //     });
-    //
-    //   })
-    //   .then(function(done, metadata){
-    //     console.log("WRITING DATABASE...");
-    //     //console.log(files);
-    //     let databasePath = createNewPath("database.json", outdir);
-    //     jsonfile.writeFile(databasePath, metadata)
-    //       .then(function(data){
-    //         console.log('write successful: %s', databasePath)
-    //       })
-    //       .catch(function(err){
-    //         console.log('json file write unsuccessful: %s',databasePath)
-    //         console.log(err);
-    //       })
-    //   });
-
+	var interval = setInterval(function() {
+		if (books === null) {
+			console.log("waiting for database...");
+		} else {
+			clearInterval(interval);
+			walkdir(dir, outdir);
+		}
+	}, 1500);
 }
 
-module.exports.processPDF = processPDF;
 module.exports.scanPDFdir = scanPDFdir;
+module.exports.scanPDF = scanPDF;
--- test/test.js
+++ test/test.js
@@ -11,9 +11,32 @@ describe('#scandir', () => {
   //   done();
   // })
 
-  it('should scan directory without crashing', (done) => {
-    scanpdf.scanPDFdir("/home/redbandit/Desktop/Projects/Wisdom", "/home/redbandit/src/projects/libManager/tinylibrarymanager/common/scanPDFdir/tmp/")
-    done();
-  })
+  // it('should scan directory without crashing', (done) => {
+  //   scanpdf.scanPDFdir("./test/librarydata/100MB", "./test/tmp/")
+  //   done();
+  // }).timeout(1000000);
 
+  // it('should scan directory without crashing', (done) => {
+  //   scanpdf.scanPDFdir("./test/librarydata/librarydata/1GB", "./test/tmp/")
+  //   done();
+  // }).timeout(1000000)
+  //
+  // it('should scan directory without crashing', (done) => {
+  //   scanpdf.scanPDFdir("./test/librarydata/5GB", "./test/tmp/")
+  //   done();
+  // }).timeout(1000000)
+  //
+  it('should scan directory without crashing', (done) => {
+    scanpdf.scanPDFdir("./test/librarydata/10GB", "./test/tmp/")
+    done();
+  }).timeout(1000000)
+
+})
+
+describe('#scanfile', () => {
+
+  // it('should scan file without crashing', (done) => {
+  //   scanpdf.scanPDF("./test/librarydata/1GB/6 Keys to Mastery.pdf", "./test/tmp/");
+  //   done();
+  // }).timeout(1000000);
 })
